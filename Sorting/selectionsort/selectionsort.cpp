656f75766b38de730521305d6f3a702ea2f42f24
xPREMy <b24bb1028@iitj.ac.in>
2025-12-19 19:12:28 +0530

    practice session

--- Sorting/selectionsort/selectionsort.cpp
+++ Sorting/selectionsort/selectionsort.cpp
@@ -5,18 +5,16 @@ void printV(const vector<int> &v){
         cout << v[i] << " ";
     }
 }
-void bubble_sort(vector<int> &v){
-    for(int i=v.size()-1;i>=1;i--){
-        int sw=0;
-        for(int j=0;j<i;j++){
-            if(v[j]>v[j+1]){
-                swap(v[j],v[j+1]);
-                sw++;
+void selection_sort(vector<int> &v){
+    for(int i=0;i<v.size()-1;i++){
+        // finding minimum element 
+        int min_i=i;
+        for(int j=i+1;j<v.size();j++){
+            if(v[min_i]>v[j]){
+                min_i=j;
             }
         }
-        if(sw==0){
-            return;
-        }
+        swap(v[i],v[min_i]);
     }
 }
 int main(){
@@ -24,13 +22,12 @@ int main(){
     int t;
     cout << "size of array :";
     cin >> t;
-    cout << "enter array : ";
     for(int i=0;i<t;i++){
         int temp;
         cin >> temp;
         v.push_back(temp);
     }
-    bubble_sort(v);
+    selection_sort(v);
     printV(v);
     return 0;
 }
